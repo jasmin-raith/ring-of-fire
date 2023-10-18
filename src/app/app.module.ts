f89d62716a4c4f1b23940dc40019aea63fbae1a5
Jasmin Raith <jasmin-raith@outlook.com>
2023-10-18 12:37:01 +0200

    start screen added and designed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartScreenComponent
   ],
   imports: [
     BrowserModule,
