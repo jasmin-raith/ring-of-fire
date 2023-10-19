c2e6bed49f201cf215bae28b7c608959d9b05246
Jasmin Raith <jasmin-raith@outlook.com>
2023-10-19 12:21:02 +0200

    animate card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  pickCardAnimation = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
   }
 
 }
