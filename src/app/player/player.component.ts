0146ad3a2f3daf717b5e86f6b1a4b5a7be17e703
Jasmin Raith <jasmin-raith@outlook.com>
2023-11-02 10:04:30 +0100

    show active player

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PlayerComponent implements OnInit {
 
   @Input() name: any;
+  @Input() playerActive: boolean = false;
 
   constructor() { }
 
