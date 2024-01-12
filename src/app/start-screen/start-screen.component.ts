333ef0b97ec29b61701c5383f75927c3943dd7b4
Jasmin Raith <jasminraith@icloud.com>
2024-01-11 23:07:27 -0300

    Create new game with unique id

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Firestore, addDoc, collection } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-start-screen',
@@ -7,16 +9,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./start-screen.component.scss']
 })
 export class StartScreenComponent implements OnInit {
-
+  firestore: Firestore = inject(Firestore);
+  game: Game = new Game;
+  
   constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
 
-  newGame() {
-    // start game
-    this.router.navigateByUrl('/game');
+  async newGame() {
+    const gamesCollection = await addDoc(collection(this.firestore, 'games'), {game: this.game.toJson()});
+    this.router.navigateByUrl('/game/' + gamesCollection.id);
   }
 
 }
