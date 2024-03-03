cfc5ae033e62a4d8d3a36b0e8e153ba8cc535553
Jasmin Raith <jasminraith@icloud.com>
2024-03-03 13:33:59 +0700

    replace cards with more beautiful ones

--- src/models/game.ts
+++ src/models/game.ts
@@ -9,7 +9,7 @@ export class Game {
 
     constructor() {
         for (let i = 1; i < 14; i++) {
-            this.stack.push('ace_' + i);
+            this.stack.push('spade_' + i);
             this.stack.push('clubs_' + i);
             this.stack.push('diamonds_' + i);
             this.stack.push('hearts_' + i);
